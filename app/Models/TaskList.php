0d989bf1a20c948a74d7d16515f0805d57979d32
Tomáš Rybenský <tomas.rybensky@gmail.com>
2023-05-11 10:00:28 +0200

    minor

--- app/Models/TaskList.php
+++ app/Models/TaskList.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TaskList extends Model
@@ -23,5 +24,10 @@ class TaskList extends Model
     {
         return $this->hasMany(Task::class)
             ->orderBy(Task::IS_COMPLETED);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
